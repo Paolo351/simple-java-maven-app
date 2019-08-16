40c82a45fafabd1e1f07fb01f1de7de9da3b64aa
Paolo351 <43697279+Paolo351@users.noreply.github.com>
2019-08-16 11:10:55 +0200

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,16 +6,28 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+	private final String message1 = "1. Hello World!";
+	private final String message2 = "2. Hello World!";
+	private final String message3 = "3. Hello World!";
 
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage1());
+	System.out.println(new App().getMessage2());
+	System.out.println(new App().getMessage3());
     }
 
-    private final String getMessage() {
-        return message;
+	private final String getMessage1() {
+        return message1;
+    }
+
+	private final String getMessage2() {
+        return message2;
+    }
+
+	private final String getMessage3() {
+        return message3;
     }
 
 }
